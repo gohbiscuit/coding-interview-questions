81478bfe4e5abc113e793f283edd0034220630c0
EDWIN\Owner <gohbiscuit@gmail.com>
2017-03-08 01:03:49 +0800

    - refined solution - added support by million, billions and trillions - added algo through online reference.

--- NumberToWord/Solution.java
+++ NumberToWord/Solution.java
@@ -4,7 +4,7 @@
 
 // 		PROBLEM
 //		Prints the word representation of an input number
-//		Number range from 1-1000000 (for this problem, million onwards is not included)
+//		Up to trillion
 
 //		Input: 4
 //		Output: Four
@@ -13,7 +13,10 @@
 //		Output: twenty three
 
 //		Input: 3214
-//		Output: Three thousand and fourteen
+//		Output: three thousand two hundred and fourteen
+
+//		Input: 1234567
+//		Output: one million two hundred and thirty four thousand five hundred and sixty seven
 
 import java.util.Scanner;
 	
@@ -33,6 +36,11 @@ public class Solution
     {
         "eleventh", "twelve", "thirteen", "fourteen", "fiveteen", "sixteen", "seventeen", "eighteen", "ninteen"
     };
+    
+    private static String[] special_digits = new String[]
+    {
+    	"", " thousand", " million", " billion", " trillion"
+    };
     	    
     
     public static void main (String[] args)
@@ -44,14 +52,14 @@ public class Solution
 	        
 	        sc.close();
 	        
-	        if(number > 0 && number < 1000000)
-	        {
-	        	System.out.println(ConvertNumberToString(number));
-	        }
-	        else
-	        {
-	        	System.out.println("Please enter positive numbers, range 1-99999 only.");
-	        }
+        	if(number >= 0)
+        	{
+        		System.out.println(ConvertNumberToString(number));
+        	}
+        	else
+        	{
+        		System.out.println("negative " + ConvertNumberToString(number));
+        	}
     	}
     	catch(NumberFormatException nfe)
     	{
@@ -59,110 +67,85 @@ public class Solution
     	}
     }
     
-    private static void PrintDebug(int digit, int ten, int hundred, int thousand, int ten_thousand, int hundred_thousand)
+    private static void PrintDebug(int digit, int ten, int hundred)
     {
     	System.out.println("digit: " + digit);
     	System.out.println("ten: " + ten);
         System.out.println("hundred: " + hundred);
-        System.out.println("thousand: " + thousand);
-        System.out.println("ten_thousand: " + ten_thousand);
-        System.out.println("hundred_thousand: " + hundred_thousand);
     }
     
+ 
     private static String ConvertNumberToString(int input)
     {
-    	int digit = input % 10;
-    	int ten = GetValueAfterDivision(input, 10);
-    	int hundred = GetValueAfterDivision(input, 100);
-    	int thousand = GetValueAfterDivision(input, 1000);
-    	int ten_thousand = GetValueAfterDivision(input, 10000);
-    	int hundred_thousand = GetValueAfterDivision(input, 100000);
-    	
-        String digits_string = "", 
-        	   ten_string = "", 
-        	   hundred_string= "", 
-        	   thousand_string = "", 
-        	   ten_thousand_string = "", 
-        	   hundred_thousand_string ="";
-        
-        
-        /* For Debug Purpose */
-        // PrintDebug(digit, ten, hundred, thousand, ten_thousand, hundred_thousand);
-        
-        
-        /* One to Nine */
-        if(input == 0)
-        {
-        	return "zero";
-        }
-        
-        if(digit > 0)
-        {
-           digits_string = single_digits[digit-1];
-        }
-        
-        /* Tenths */
-        if(ten == 1)
-        {
-        	ten_string = digit == 0 ? tens[0] : ten_single_digits[digit-1]; 
-        	ten_string += " ";
-           
-            digits_string = "";
-        }
-        
-        if(ten > 1 && ten < 10)
-        {
-            ten_string = tens[ten-1] + " ";
-        }
+    	int thousands_index = 0;
+    	String word = "";
+    	do
+    	{
+    		/* get the remainder less than a thousand */
+    		int num = input % 1000;
 
-        /* Hundreds */
-        if(hundred > 0 && hundred < 10)
-        {
-            hundred_string = single_digits[hundred-1];
-            hundred_string += (digit > 0 || ten > 0) ? " hundred and " : " hundred ";
-        }
-      
-        
-        /* Thousand */
-        if(thousand == 1)
-        {
-        	thousand_string = ten_single_digits[hundred-1];
-        	thousand_string += " thousand ";
-        }
-        
-        if(thousand > 1 && thousand < 10)
-        {
-            thousand_string = single_digits[thousand-1];
-            thousand_string += " thousand ";
-        }
-        
-        /* Ten Thousand  */
-        if(ten_thousand == 1)
-        {
-        	ten_thousand_string = ten_single_digits[thousand-1];
-        	ten_thousand_string += " thousand ";
-        	
-        	// clear 
-        	thousand_string = "";
-        }
-        
-        if(ten_thousand > 1 && ten_thousand < 10)
-        {
-        	ten_thousand_string = tens[ten_thousand-1];
-        	ten_thousand_string += thousand == 0 ? " thousand " : " ";
-        }
-        
-        /* Hundred Thousand  */
-        if(hundred_thousand >= 1 && hundred_thousand < 10)
-        {
-            hundred_thousand_string = single_digits[hundred_thousand-1];
-            hundred_thousand_string += " hundred ";
-            hundred_thousand_string += ten_thousand == 0 ? "thousand " : "";
-        }
-        
-        return hundred_thousand_string + ten_thousand_string + thousand_string + hundred_string + ten_string + digits_string;
+    		/* convert less than a thousand */
+    		if(num != 0)
+    		{
+    			String lessThanOneThousand = ConvertLessThanOneThousand(num);
+   
+    			word = lessThanOneThousand + special_digits[thousands_index] + word;
+    		}
+    		
+    		thousands_index++;
+    		input /= 1000;
+    		
+    	}while(input > 0);
+    	
+    	return word.trim();
     }
     
+    private static String ConvertLessThanOneThousand(int num)
+    {
+    	int digit = num % 10;
+    	int ten = GetValueAfterDivision(num, 10);
+    	int hundred = GetValueAfterDivision(num, 100);
+    	
+    	String digits_string = "", ten_string = "", hundred_string= "";
+    	
+		 /* For Debug Purpose */
+		 //PrintDebug(digit, ten, hundred);
+		  
+		 /* One to Nine */
+		 if(num == 0)
+		 {
+			 return "zero";
+		 }
+		  
+		 if(digit > 0)
+		 {
+			 digits_string = single_digits[digit-1];
+		 }
+		  
+		 /* Tenths */
+		 if(ten == 1)
+		 {
+			 ten_string = digit == 0 ? tens[0] : ten_single_digits[digit-1]; 
+			 ten_string += " ";
+		 
+			 digits_string = "";
+		 }
+		  
+		 if(ten > 1 && ten < 10)
+		 {
+			 ten_string = tens[ten-1] + " ";
+		 }
+		
+		 /* Hundreds */
+		 if(hundred > 0 && hundred < 10)
+		 {
+			 hundred_string = single_digits[hundred-1];
+			 hundred_string += (digit > 0 || ten > 0) ? " hundred and " : " hundred ";
+		 }
+
+		 return " " + hundred_string + ten_string + digits_string;
+    }
+ 
     /* Gets the value after division
      * E.g. GetValueAfterDivision(1234, 10) --> it will extract out 34/10 and returns a value of 3
      * 		GetValueAfterDivision(1234, 100) --> it will extract out 234/100 and returns a value of 2
